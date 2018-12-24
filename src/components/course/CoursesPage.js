1b3b5eb517b97200bbf9e1c6aa055d0c248027b0
River Phillips <riverphillips1@gmail.com>
2018-12-24 16:54:04 +0000

    Display list of courses from mock api

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -10,42 +10,13 @@ class CoursesPage extends React.Component {
 
   constructor(props, context) {
     super(props, context);
-    this.state = {
-      course: {"title": ""}
-    };
-
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onClickSave = this.onClickSave.bind(this);
-  }
-
-  onTitleChange(event) {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({course: course});
-  }
-
-  onClickSave() {
-    this.props.actions.createCourse(this.state.course);
   }
 
   render() {
     return (
       <div>
         <h1>Courses</h1>
-
         {this.props.courses.map(CoursesPage.courseRow)}
-
-        <h2>Add new course</h2>
-        <input
-          type="text"
-          onChange={this.onTitleChange}
-          value={this.state.course.title}/>
-
-        <input
-          type="submit"
-          value="Save"
-          onClick={this.onClickSave}
-        />
       </div>
     );
   }
